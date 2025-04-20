f3568ca5ffa337423a428e555736c71d7949cc63
Jhonny <ramirezjhonny64@gmail.com>
2025-04-20 01:51:54 -0500

    Refactorizar la inicialización de la aplicación para verificar el estado de autenticación antes del montaje

--- src/main.js
+++ src/main.js
@@ -2,26 +2,33 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
-import Toast from 'vue-toastification'
-import 'vue-toastification/dist/index.css'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import '@/assets/main.css';
-
-const app = createApp(App);
+import { auth } from '@/firebase/config'; // Importa la configuración de Firebase
+import { onAuthStateChanged } from 'firebase/auth';
 
 const toastOptions = {
-    timeout: 5000,
-    closeOnClick: true,
-    pauseOnFocusLoss: true,
-    pauseOnHover: true,
-    draggable: true,
-    draggablePercent: 0.6,
-    showCloseButtonOnHover: false,
-    hideProgressBar: true,
-    icon: true,
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnFocusLoss: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+  showCloseButtonOnHover: false,
+  hideProgressBar: true,
+  icon: true,
+};
+
+let app;
+
+// Verifica el estado de autenticación antes de montar la aplicación
+onAuthStateChanged(auth, () => {
+  if (!app) {
+    app = createApp(App);
+    app.use(createPinia());
+    app.use(router);
+    app.use(Toast, toastOptions);
+    app.mount('#app');
   }
-
-app.use(createPinia());
-app.use(router);
-app.use(Toast, toastOptions)
-
-app.mount('#app');
+});
